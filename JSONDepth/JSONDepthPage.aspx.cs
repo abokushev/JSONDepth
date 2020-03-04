392db70359f02dd70cbb57e33c46a14ff0f55807
abokushev <a.bokushev@gmail.com>
2020-03-04 18:19:50 +0600

    Initial commit

---
+++ JSONDepth/JSONDepthPage.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JSONDepth
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected int maxDepth = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            JSONOutput.Text = Output("");//JSONInput.Text);
+        }
+
+        public String Output(string input)
+        {
+            JObject jsonRoot = null;
+            try
+            {
+                jsonRoot = JsonConvert.DeserializeObject<JObject>(input);
+                if(jsonRoot == null)
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception ex)
+            {    
+                return "{\"error\": \"Invalid JSON\"}";
+            }           
+            JSONDepth(jsonRoot, maxDepth);
+            return "{\"levels\" : " + maxDepth.ToString() + '}';            
+        }
+
+        private void JSONDepth(JToken objTree, int iChildDepth)
+        {           
+            iChildDepth++;            
+            if (objTree.Values().Count() > 0)
+            {
+                if (maxDepth < iChildDepth)
+                {
+                    maxDepth = iChildDepth;
+                }
+                foreach (JToken jt in objTree.Values())
+                {
+                    JSONDepth(jt, iChildDepth);
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ JSONDepthTest/JSONDepthTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JSONDepth;
+
+namespace JSONDepthTest
+{
+    [TestClass]
+    public class JSONDepthTests
+    {
+        //check JSON with depth 4
+        [TestMethod]
+        public void DepthCase4()
+        {
+            string input = "{\"identity\": {\"name\": \"Test\", \"translations\": [{\"order\": 1, \"language\": \"ru\", \"value\": \"Тест\"}]}}";
+            string output = "{\"levels\" : 4}";
+
+            WebForm1 wf = new WebForm1();
+            Assert.AreEqual(output, wf.Output(input));
+        }
+
+        //check JSON with depth 1
+        [TestMethod]
+        public void DepthCase1()
+        {
+            string input = "{ \"key\": \"value\"}";
+            string output = "{\"levels\" : 1}";
+
+            WebForm1 wf = new WebForm1();
+            Assert.AreEqual(output, wf.Output(input));
+        }
+
+        //check invalid JSON
+        [TestMethod]
+        public void JSONCaseInvalid()
+        {
+            string input = "{\"incorrect: \"value\"}";
+            string output = "{\"error\": \"Invalid JSON\"}";
+
+            WebForm1 wf = new WebForm1();
+            Assert.AreEqual(output, wf.Output(input));
+        }
+
+        [TestMethod]
+        public void JSONCaseEmptyInput()
+        {
+            string input = "";
+            string output = "{\"error\": \"Invalid JSON\"}";
+
+            WebForm1 wf = new WebForm1();
+            Assert.AreEqual(output, wf.Output(input));
+        }
+    }
+}
